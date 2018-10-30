c61f6bb785061a0c925a1c93330d94561b81e6f3
Artem Samofalov <dex157@gmail.com>
2018-10-30 13:45:43 +0300

    add demo

---
+++ demo/precache-manifest.fef6a0f639b9c8dada33186afd556cac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b334e72c067de5e81d8e",
+    "url": "/static/js/main.b334e72c.chunk.js"
+  },
+  {
+    "revision": "44b5aa4f9c8bfcfde756",
+    "url": "/static/js/1.44b5aa4f.chunk.js"
+  },
+  {
+    "revision": "b334e72c067de5e81d8e",
+    "url": "/static/css/main.ea6f3254.chunk.css"
+  },
+  {
+    "revision": "1a7944c113720f203eb76cacb7436a0c",
+    "url": "/index.html"
+  }
+];
